6c065cf6f0e6378e3029ebc38b4ae138093a2429
dev-code-create <ayushkankale04@gmail.com>
2025-04-17 20:53:43 +0530

    validation done

---
+++ Backend/middleware/validateRequest.js
@@ -0,0 +1,22 @@
+import { validationResult } from "express-validator";
+import { body } from "express-validator";
+export const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
+
+export const signupRules = [
+  body("name", "Name is required").notEmpty(),
+  body("email", "Please include a valid email").isEmail(),
+  body("password", "Password must be 6 or more characters").isLength({
+    min: 6,
+  }),
+];
+
+export const loginRules = [
+  body("email", "Please include a valid email").isEmail(),
+  body("password", "Password is required").exists(),
+];
--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { login, signup } from "../controller/authController.js";
+import { signupRules, validate } from "../middleware/validateRequest.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/signin", login);
+router.post("/signup", signupRules, validate, signup);
+router.post("/signin", signupRules, validate, login);
 
 export default router;
